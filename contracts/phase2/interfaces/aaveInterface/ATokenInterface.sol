058a0b4b6a790fdda6a17d96c4106d8b4d700dd2
sunnyRK <radadiyasunny970@gmail.com.com>
2020-08-13 09:38:11 +0530

    Revert "ADded readme"
    This reverts commit 73d7263323782b77237afb210595d75e199ed082.

--- contracts/phase2/interfaces/aaveInterface/ATokenInterface.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.6.0;
-
-interface AaveCoreInterface {
-    function getReserveATokenAddress(address _reserve) external view returns (address);
-}
--- contracts/phase2/interfaces/chinlinkInterface/IchainlinkAlarm.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.6.0;
-
-interface IchainlinkAlarm {
-    function delayStart(address _oracle, string calldata _jobId, uint256 min) external returns(bytes32 _requestId);  
-    function getUint() external view returns(uint256);
-}
